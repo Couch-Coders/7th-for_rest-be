d0cbc80e8c17e2ee279df55e95ade71beaa78d80
Godric <kimdeiz@naver.com>
2022-03-27 15:47:06 +0900

    BUILD: 로그인구현

--- src/main/java/couch/forrest/domain/member/entity/Member.java
+++ src/main/java/couch/forrest/domain/member/entity/Member.java
@@ -2,6 +2,8 @@ package couch.forrest.domain.member.entity;
 
 import couch.forrest.domain.base.BaseTimeEntity;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
 import java.util.Collection;
@@ -11,7 +13,7 @@ import java.util.Collection;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class Member extends BaseTimeEntity {
+public class Member extends BaseTimeEntity implements UserDetails {
 
     @Id @GeneratedValue
     @Column(name = "member_id")
@@ -21,4 +23,38 @@ public class Member extends BaseTimeEntity {
     private String picture;
 
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
 }
