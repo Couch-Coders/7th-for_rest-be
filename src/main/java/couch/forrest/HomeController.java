84a9adb5071ac5d6a2ae6b97dee947a322e7bd0d
Sunggwang Park <54604123+Sunggwang-Park@users.noreply.github.com>
2022-03-31 12:23:32 +0900

    FIX: HomeController 수정

--- src/main/java/couch/forrest/HomeController.java
+++ src/main/java/couch/forrest/HomeController.java
@@ -1,13 +1,14 @@
 package couch.forrest;
 
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
 
-    @RequestMapping("/")
+    @GetMapping("/")
     public String home() {
         return "this is for-rest";
     }
