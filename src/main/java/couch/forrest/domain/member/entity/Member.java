511c92bdb0cdf410fb02842adec68b367a385f34
Sunggwang Park <54604123+Sunggwang-Park@users.noreply.github.com>
2022-03-31 10:34:31 +0900

    CHORE: OAuth 부분 주석 제거

--- src/main/java/couch/forrest/domain/member/entity/Member.java
+++ src/main/java/couch/forrest/domain/member/entity/Member.java
@@ -1,60 +1,60 @@
-//package couch.forrest.domain.member.entity;
-//
-//import couch.forrest.domain.base.BaseTimeEntity;
-//import lombok.*;
-//import org.springframework.security.core.GrantedAuthority;
-//import org.springframework.security.core.userdetails.UserDetails;
-//
-//import javax.persistence.*;
-//import java.util.Collection;
-//
-//@Entity
-//@Getter
-//@NoArgsConstructor
-//@AllArgsConstructor
-//@Builder
-//public class Member extends BaseTimeEntity implements UserDetails {
-//
-//    @Id @GeneratedValue
-//    @Column(name = "member_id")
-//    private Long id;
-//    private String email;
-//    private String name;
-//    private String picture;
-//
-//
-//    @Override
-//    public Collection<? extends GrantedAuthority> getAuthorities() {
-//        return null;
-//    }
-//
-//    @Override
-//    public String getPassword() {
-//        return null;
-//    }
-//
-//    @Override
-//    public String getUsername() {
-//        return email;
-//    }
-//
-//    @Override
-//    public boolean isAccountNonExpired() {
-//        return false;
-//    }
-//
-//    @Override
-//    public boolean isAccountNonLocked() {
-//        return false;
-//    }
-//
-//    @Override
-//    public boolean isCredentialsNonExpired() {
-//        return false;
-//    }
-//
-//    @Override
-//    public boolean isEnabled() {
-//        return false;
-//    }
-//}
+package couch.forrest.domain.member.entity;
+
+import couch.forrest.domain.base.BaseTimeEntity;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import java.util.Collection;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Member extends BaseTimeEntity implements UserDetails {
+
+    @Id @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+    private String email;
+    private String name;
+    private String picture;
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+}
