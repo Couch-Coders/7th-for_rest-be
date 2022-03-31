31a46a3e6cc40dc928edee01053849cc773ca02b
Sunggwang Park <54604123+Sunggwang-Park@users.noreply.github.com>
2022-03-31 14:16:39 +0900

    Revert "FIX: HomeController 수정"
    This reverts commit 84a9adb5071ac5d6a2ae6b97dee947a322e7bd0d.

--- src/main/java/couch/forrest/HomeController.java
+++ src/main/java/couch/forrest/HomeController.java
@@ -1,14 +1,13 @@
 package couch.forrest;
 
 
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
 
-    @GetMapping("/")
+    @RequestMapping("/")
     public String home() {
         return "this is for-rest";
     }
