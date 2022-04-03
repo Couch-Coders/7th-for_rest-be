edfde08d3caf9e17819cde24cc884d1fa60e8924
Godric <kimdeiz@naver.com>
2022-04-03 14:49:29 +0900

    fix: Like 테이블 Love 테이블로 변경
    sql 명령어 like와 겹쳐서 오류 발생

--- src/main/java/couch/forrest/domain/like/dao/LikeRepository.java
+++
@@ -1,9 +0,0 @@
-package couch.forrest.domain.like.dao;
-
-import couch.forrest.domain.like.entity.Like;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface LikeRepository extends JpaRepository<Like, Long> {
-}
