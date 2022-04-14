88c3719c037884a4ca34ee622751f2a934604b4f
Godric <kimdeiz@naver.com>
2022-04-14 22:10:37 +0900

    chore: 파이어베이스 컨피그 파일 수정
    admin key 위치 변경

--- src/main/java/couch/forrest/config/FirebaseConfig.java
+++ src/main/java/couch/forrest/config/FirebaseConfig.java
@@ -1,9 +1,11 @@
 package couch.forrest.config;
 
 import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.storage.Bucket;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.cloud.StorageClient;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -29,4 +31,14 @@ public class FirebaseConfig {
         FirebaseApp app = FirebaseApp.initializeApp(options);
         return app;
     }
+
+    @Bean
+    public FirebaseAuth firebaseAuth() throws IOException {
+        return FirebaseAuth.getInstance(firebaseApp());
+    }
+
+//    @Bean
+//    public Bucket bucket() throws IOException {
+//        return StorageClient.getInstance(firebaseApp()).bucket();
+//    }
 }
