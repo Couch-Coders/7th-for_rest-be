ed7cb95a666f8562581e14979ea26aca1f6845ad
Godric <kimdeiz@naver.com>
2022-04-16 15:28:34 +0900

    test: 댓글등록_테스트_코드_작성
    댓글 등록 테스트 코드가 추가 되었습니다.

---
+++ src/main/java/couch/forrest/domain/review/controller/ReviewApiController.java
@@ -0,0 +1,43 @@
+package couch.forrest.domain.review.controller;
+
+import couch.forrest.domain.member.entity.Member;
+import couch.forrest.domain.review.dto.request.ReviewSaveRequestDto;
+import couch.forrest.domain.review.entity.Review;
+import couch.forrest.domain.review.service.ReviewService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Controller
+public class ReviewApiController {
+    private final ReviewService reviewService;
+
+    @ResponseBody
+    @PostMapping("/reviews")
+    public Long save(@RequestBody ReviewSaveRequestDto requestDto, @AuthenticationPrincipal Member member) {
+        return reviewService.save(requestDto, member);
+    }
+
+    @ResponseBody
+    @PatchMapping("/reviews/{reviewId}")
+    public Long update(@PathVariable Long id, @RequestBody ReviewSaveRequestDto requestDto, @AuthenticationPrincipal Member member)
+    {
+        return reviewService.update(id, requestDto,member);
+    }
+
+    @DeleteMapping("/reviews/{reviewId}")
+    public Long delete(@PathVariable Long id, @AuthenticationPrincipal Member member) {
+        reviewService.delete(id,member);
+        return id;
+    }
+
+    @GetMapping("/reviews/{placeId}/with-place")
+    public List<Review> loadAllReview(@PathVariable Long id) {
+        return reviewService.loadAllReview(id);
+    }
+
+}
